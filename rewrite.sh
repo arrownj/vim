4e6f4f13ef8015b04beb699671f6a0004ecd4b34
xiaojian <xiaojian.xu@uisee.com>
2017-04-10 09:12:30 +0800

    update clone color scheme

--- rewrite.sh
+++ rewrite.sh
@@ -3,4 +3,4 @@ cp -f .vimrc ..
 cp -rf .vim ..
 cp -rf .ssh ..
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/tpope/vim-vividchalk.git ~/.vim/colors
+git clone https://github.com/tpope/vim-vividchalk.git ~/.vim
