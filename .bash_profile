b504f143be06e5bcb56d61ab47061437e9511987
Xiaojian Xu <xiaojian.xu@uisee.com>
2017-12-01 10:54:43 +0800

    new version

--- .bash_profile
+++
@@ -1,6 +0,0 @@
-export CLICOLOR=1
-export LSCOLORS=gxfxcxdxbxegedabagacad
-export PS1="\[\033[01;32m\]\W\[\033[00m\]\$ "
-
-alias sshmj="ssh root@112.124.16.226"
-alias ssharrow="ssh root@121.42.13.192"
--- rewrite.sh
+++ rewrite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -f .vimrc ..
-cp -rf .vim ..
+
+mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tpope/vim-vividchalk.git ~/.vim/colors
 mv ~/.vim/colors/colors/* ~/.vim/colors
