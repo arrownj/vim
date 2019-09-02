e09174ad6f4a8eb7fb649b365aa819c57af776d7
Arrow Xu <arrownj@126.com>
2019-09-02 17:30:09 +0800

    Update rewrite.sh

--- rewrite.sh
+++ rewrite.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tpope/vim-vividchalk.git ~/.vim/colors
